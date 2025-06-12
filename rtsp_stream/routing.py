73cb0f516cd6253696573abec101b01ac26ff286
root <root@DESKTOP-VPRD95P.localdomain>
2025-06-12 02:43:30 +0000

    routing added wss

--- rtsp_stream/routing.py
+++ rtsp_stream/routing.py
@@ -2,8 +2,8 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/metadata/$', consumers.FFmpegMetadataConsumer.as_asgi()),
-    re_path(r'ws/faces/$', consumers.FaceMetadataConsumer1.as_asgi()),
+    re_path(r'wss/metadata/$', consumers.FFmpegMetadataConsumer.as_asgi()),
+    re_path(r'wss/faces/$', consumers.FaceMetadataConsumer1.as_asgi()),
     # re_path(r'ws/faces/(?P<stream_id>\w+)/$', consumers.FaceMetadataConsumer1.as_asgi()),
 ]
 
